8c288d43bc7463dc0f1f169c5f19f0be53ffeaac
Gabriel Majeri <gabriel.majeri6@gmail.com>
2024-02-18 12:02:54 +0200

    Change user account used for testing

--- config/postgresql/init.sql
+++ config/postgresql/init.sql
@@ -1,2 +1,2 @@
-CREATE USER "admin@x8dw7.onmicrosoft.com";
-GRANT ALL PRIVILEGES ON DATABASE postgres TO "admin@x8dw7.onmicrosoft.com";
+CREATE USER "test@x8dw7.onmicrosoft.com";
+GRANT ALL PRIVILEGES ON DATABASE postgres TO "test@x8dw7.onmicrosoft.com";
